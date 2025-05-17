4401527863ab15acf3c0a8c282f95d70b3dafbd3
Avan <avansingh085@gmail.com>
2025-05-18 02:15:28 +0530

    update

--- src/components/ProtectRoutes.jsx
+++ src/components/ProtectRoutes.jsx
@@ -2,7 +2,7 @@ import { Navigate } from 'react-router-dom';
 import { isLoggedIn } from '../services/authService';
 import { useSelector } from 'react-redux';
 const ProtectedRoute = ({ children }) => {
-  console.log(useSelector((state)=>state.user))
+
   if (!useSelector((state)=>state.user.isLogin)) {
     return <Navigate to="/Login" replace />;
   }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,6 +13,7 @@ const AuthForm = () => {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [errors, setErrors] = useState({});
+  const [fetchAuth,setfetchAuth]=useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const validateForm = () => {
@@ -36,12 +37,13 @@ const loading=useSelector((state)=>state.user.loading)
     if (!validateForm()) return;
 
     try {
+      fetchAuth(true);
       let response = await apiClient.post(isLogin ? "/api/auth/login" : "/api/auth/register", {
         email,
         password,
         ...(isLogin ? {} : { name }),
       });
-      console.log(response.data)
+      
       if (response.data.success) {
         setToken(response.data.token);
         Toast.Success('login successfully!')
@@ -54,6 +56,8 @@ const loading=useSelector((state)=>state.user.loading)
       }
     } catch (error) {
       console.log(error);
+    }finally{
+      setfetchAuth(false);
     }
   };
 
@@ -130,6 +134,7 @@ const loading=useSelector((state)=>state.user.loading)
             <div>
               <button
                 type="submit"
+                disabled={fetchAuth}
                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700"
               >
                 {isLogin ? 'Sign in' : 'Sign up'}
@@ -146,6 +151,7 @@ const loading=useSelector((state)=>state.user.loading)
 
             <button
               onClick={() => setIsLogin(!isLogin)}
+              
               className="w-full mt-4 flex justify-center py-2 px-4 border rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-gray-50"
             >
               {isLogin ? 'Create a new account' : 'Sign in instead'}
