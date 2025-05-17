2ec89ce2f0b10172c5c286a70266339576b98a46
Avan <avansingh085@gmail.com>
2025-05-18 02:04:02 +0530

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,7 +27,10 @@ const AuthForm = () => {
   };
 const login=useSelector((state)=>state.user.isLogin)
 const loading=useSelector((state)=>state.user.loading)
- 
+ useEffect(()=>{
+  if(login)
+    navigate('/Game');
+ },[login])
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!validateForm()) return;
@@ -55,9 +58,9 @@ const loading=useSelector((state)=>state.user.loading)
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="min-h-screen rounded-tl-full rounded-br-full   bg-amber-200 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-extrabold bg-green-400">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-green-400 animate-bounce ">
           {isLogin ? 'Sign in to your account' : 'Create a new account'}
         </h2>
       </div>
@@ -66,26 +69,28 @@ const loading=useSelector((state)=>state.user.loading)
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
           <form className="space-y-6" onSubmit={handleSubmit}>
             <div>
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email address</label>
+              
               <input
                 id="email"
                 type="email"
                 required
-                className={`appearance-none block w-full px-3 py-2 border ${errors.email ? 'border-red-500' : 'border-gray-300'} rounded-md shadow-sm`}
+                className={`appearance-none border-t-0 border-x-0 outline-none border-blue-500 border-b-4 block w-full px-3 py-2 border ${errors.email ? 'border-red-500' : 'border-gray-300'} rounded-md shadow-sm`}
                 value={email}
+                placeholder='email'
                 onChange={(e) => setEmail(e.target.value)}
               />
               {errors.email && <p className="text-sm text-red-600">{errors.email}</p>}
             </div>
 
             <div>
-              <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+              
               <input
                 id="password"
                 type="password"
                 required
-                className={`appearance-none block w-full px-3 py-2 border ${errors.password ? 'border-red-500' : 'border-gray-300'} rounded-md shadow-sm`}
+                className={`appearance-none border-t-0 border-x-0 outline-none border-blue-500 border-b-4 block w-full px-3 py-2 border ${errors.password ? 'border-red-500' : 'border-gray-300'} rounded-md shadow-sm`}
                 value={password}
+                placeholder='password'
                 onChange={(e) => setPassword(e.target.value)}
               />
               {errors.password && <p className="text-sm text-red-600">{errors.password}</p>}
@@ -94,25 +99,27 @@ const loading=useSelector((state)=>state.user.loading)
             {!isLogin && (
               <>
                 <div>
-                  <label htmlFor="username" className="block text-sm font-medium text-gray-700">Name</label>
+                  
                   <input
                     id="name"
                     type="text"
                     required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
+                    className="appearance-none border-t-0 border-x-0 outline-none border-blue-500 border-b-4 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
                     value={name}
+                    placeholder='username'
                     onChange={(e) => setName(e.target.value)}
                   />
                 </div>
 
                 <div>
-                  <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">Confirm Password</label>
+                  
                   <input
                     id="confirmPassword"
                     type="password"
                     required
-                    className={`appearance-none block w-full px-3 py-2 border ${errors.confirmPassword ? 'border-red-500' : 'border-gray-300'} rounded-md shadow-sm`}
+                    className={`appearance-none block border-t-0 border-x-0 outline-none border-blue-500 border-b-4 w-full px-3 py-2 border ${errors.confirmPassword ? 'border-red-500' : 'border-gray-300'} rounded-md shadow-sm`}
                     value={confirmPassword}
+                    placeholder='password'
                     onChange={(e) => setConfirmPassword(e.target.value)}
                   />
                   {errors.confirmPassword && <p className="text-sm text-red-600">{errors.confirmPassword}</p>}
